39072c9535a5759be1f707153291b61816fb2197
Brian Pritt <brianpritt@gmail.com>
2017-03-03 14:27:28 -0800

    add styling to portfolio page

--- src/DeveloperProfile/wwwroot/js/Portfolio.js
+++ src/DeveloperProfile/wwwroot/js/Portfolio.js
@@ -6,8 +6,9 @@
         datatype: 'json',
         success: function (result) {
             $.each(result, function (eachOne, project){
-                $("#starred").append("<p>" + project.name + "</p>")
+                $("#starred").append("<div class='col s12 m5 colors'><div class='card-panel transparent '><span class='white-text'><h5>Project Name: " +project.name + "</h5><p>Description: "+project.description+"<p><a href='https://github.com/"+project.full_name+"'>Project Repo</a></p></span></div></div>")
             });
         }
     });
 });
+
